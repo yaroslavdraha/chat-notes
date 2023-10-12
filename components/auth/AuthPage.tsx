6d6f2420f8063514727f858d45d509e0b295c1ad
Yaroslav Draha <yaroslav.draha@tui.com>
2023-10-12 11:48:30 +0300

    add logo

--- components/auth/AuthPage.tsx
+++ components/auth/AuthPage.tsx
@@ -6,6 +6,9 @@ import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { useTheme } from 'next-themes'
+import Image from "next/image";
+import logoLight from '@/public/images/logo-light.png'
+import logoDark from '@/public/images/logo-dark.png'
 
 const supabase = createClientComponentClient()
 
@@ -25,8 +28,8 @@ export const AuthPage = (): JSX.Element => {
     <div className="py-10 px-2">
       <div className="w-[300px]">
         <div className="py-4 text-center">
-          <p className="text-2xl">Welcome to ChatNotes!</p>
-          <p className="text-sm">Please sign in or sign up to use the app</p>
+          <Image src={theme === 'dark' ? logoLight : logoDark} alt="Chat Notes logo" />
+          <p className="text-sm mt-20">Please sign in or sign up to use the app</p>
         </div>
 
         <Auth
